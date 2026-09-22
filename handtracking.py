254caac50ae15399673659ffa87483adff1260d7
Harjot Singh <Harjotmatharu16@gmail.com>
2026-09-22 14:39:00 -0600

    Coded the hand tracking

--- handtracking.py
+++ handtracking.py
@@ -1,3 +1,50 @@
 import cv2
 import mediapipe as mp
 import time
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands 
+hands = mpHands.Hands() 
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+
+
+while True:
+    
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+            h, w, c = img.shape
+            for id, lm in enumerate(handLms.landmark):
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                cv2.putText(img, str(id), (cx + 5, cy - 5), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+                
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+
+    cv2.imshow("Image", img)
+
+    # Quit on 'q' key
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
+cv2.waitKey(1)  # macOS needs this to actually close the window
